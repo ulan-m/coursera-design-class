547a8984bd46e8d9822b7170f67d1d7d0907a917
Ulan Murzatayev <murzatayev@gmail.com>
2014-04-26 01:17:39 +0600

    added dist

---
+++ dist/scripts/9d621b39.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("courseraDesignClassApp",[]),angular.module("courseraDesignClassApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
